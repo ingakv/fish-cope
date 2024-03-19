f19df75c2be503f38b6c6129d93a87665c5958a2
ingakv <ingakv@stud.ntnu.no>
2024-03-19 10:05:04 +0100

    Species have different color outlines

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PIL import Image, ImageDraw
 annotation_folder = "../ANADROM/Annotation/"
 
 annotations_data = []
+species_data = []
 
 temp_path = 'temp/'
 modified_image_path = 'modified_images/'
@@ -29,6 +30,19 @@ for zip_file in os.listdir(annotation_folder):
 
                 tree = ET.parse(os.path.join(temp_path, file))
                 root = tree.getroot()
+
+                labels = root.find(".//labels")
+
+
+                for label in labels.findall("./label"):
+                    species = label.find("name")
+                    color = label.find("color")
+
+                    # Save data about the fish species and box color
+                    species_data.append((zip_file, species, color))
+
+
+
                 for track in root.findall("./track"):
                     species = track.get("label")
                     for box in track.findall("./box"):
@@ -56,7 +70,17 @@ for zip_file in os.listdir(annotation_folder):
 # Read Image Data and Overlay Annotations
 for annotation in annotations_data:
 
+    all_species = []
+
     zip_path, frame_number, species, (xtl, ytl, xbr, ybr) = annotation
+
+    # Add all the species and their box color in the current xml
+    for elem in species_data:
+        species_zip_path, _, _ = elem
+        if species_zip_path == zip_path:
+            _, fish_species, color = elem
+            all_species.append((fish_species, color))
+
 
     image_path = os.path.join(annotation_folder, zip_path)
 
@@ -67,6 +91,7 @@ for annotation in annotations_data:
             # Get the file from the zip
             file_list = [file for file in zip_ref.namelist() if os.path.basename(file) == f"frame_{frame_number:06d}.PNG"]
 
+            # Only one file should be found
             if len(file_list) > 1:
                 print(f"Error: More than one file found for {image_path}")
 
@@ -81,7 +106,20 @@ for annotation in annotations_data:
                 # Draw the box around the fish
                 image = Image.open(temp_path + image_file)
                 draw = ImageDraw.Draw(image)
-                draw.rectangle([(xtl, ytl), (xbr, ybr)], outline="red")
+
+                # Default to None if the species is not found
+                species_color = None
+
+                for s, c in all_species:
+                    if s.text == species:
+                        species_color = c
+                        break
+
+                if species_color is None:
+                    print(f"Error: No color found for {species}")
+
+
+                draw.rectangle([(xtl, ytl), (xbr, ybr)], outline=species_color.text)
 
                 # Save the modified image with annotations
                 modifier = 0
