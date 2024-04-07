04722da9effa5b91d61c1df6abe28048107558a6
ferdinws <ferdinws@stud.ntnu.no>
2024-04-07 22:23:34 +0200

    For loops in python. putting everything in a for loop for more zip files

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ import shutil
 import zipfile
 import xml.etree.ElementTree as ET
 
+
 def label_to_int(str):
-    if str == "SEATROUT": return 0
-    elif str == "SMOLT": return 1
-    elif str == "SALMON": return 2
-    elif str == "SEACHAR": return 3
-    return 4
+    if str == "FISH":
+        return 0
+    elif str == "SEATROUT":
+        return 1
+    elif str == "SMOLT":
+        return 2
+    elif str == "SALMON":
+        return 3
+    elif str == "SEACHAR":
+        return 4
+    return 5
 
 
 def unzip_files(zip_file, extract_folder):
@@ -21,7 +28,7 @@ def unzip_files(zip_file, extract_folder):
         zip_ref.extractall(extract_folder)
 
 
-def convert_to_yolo(xml_file, images_folder, output_folder):
+def convert_to_yolo(xml_file, output_folder, index):
     # Parse XML file
     tree = ET.parse(xml_file)
     root = tree.getroot()
@@ -48,30 +55,42 @@ def convert_to_yolo(xml_file, images_folder, output_folder):
             height = (ybr - ytl) / image_height
 
             # Write YOLO-NAS format to text file
-            txt_filename = f"{output_folder}/frame_{frame:06d}.txt"
+            txt_filename = f"{output_folder}/{index}frame_{frame:06d}.txt"
             with open(txt_filename, 'a') as txt_file:
                 txt_file.write(f"{label} {x_center} {y_center} {width} {height}\n")
 
 
 if __name__ == "__main__":
 
-    zip_file = "test_images/task_fish_detection-2024_02_05_18_43_34-cvat for video 1.1.zip"
-    extract_folder = "extracted_data"
-    xml_file = f"{extract_folder}/annotations.xml"
-    images_folder = f"{extract_folder}/images"
-    output_folder = "yolo_nas_output"
+    zip_files = ["test_images/task_fish_detection-2024_01_28_18_33_21-cvat for video 1.1.zip",
+                 "test_images/task_fish_detection-2024_02_05_18_43_34-cvat for video 1.1.zip",
+                 "test_images/task_fish_detection-2024_02_05_11_51_48-cvat for video 1.1.zip"]
 
-    # Unzip files
-    unzip_files(zip_file, extract_folder)
+    # zip_file = "test_images/task_fish_detection-2024_01_28_18_33_21-cvat for video 1.1.zip"
+    # zip_file = "test_images/task_fish_detection-2024_02_05_18_43_34-cvat for video 1.1.zip"
+    # zip_file = "test_images/task_fish_detection-2024_02_05_11_51_48-cvat for video 1.1.zip"
 
-    # Convert to YOLO-NAS format
-    convert_to_yolo(xml_file, images_folder, output_folder)
+    output_folder_txt = "yolo_txt"
 
-    files = os.listdir(images_folder)
+    output_folder_img = "yolo_img"
 
-    # iterating over all the files in
-    # the source directory
-    for fname in files:
-        # copying the files to the
-        # destination directory
-        shutil.copy2(os.path.join(images_folder, fname), output_folder)
+    if not os.path.exists(output_folder_txt):
+        os.makedirs(output_folder_txt)
+
+    if not os.path.exists(output_folder_img):
+        os.makedirs(output_folder_img)
+
+    for i, zip_file in zip_files:
+        extract_folder = "extracted_data" + i
+        if not os.path.exists(extract_folder):
+            os.makedirs(extract_folder)
+
+        unzip_files(zip_file, extract_folder)
+        xml_file = f"{extract_folder}/annotations.xml"
+        images_folder = f"{extract_folder}/images"
+        convert_to_yolo(xml_file, output_folder_txt, i)
+        files = os.listdir(images_folder)
+
+        for fileName in enumerate(os.listdir(images_folder)):
+            new_filename = f"{i}_{fileName}"
+            shutil.copy2(os.path.join(images_folder, fileName), os.path.join(output_folder_img, new_filename))
