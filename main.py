9cc3f77f75cf0381dbaed0b04ec8945f23364851
ferdinws <ferdinws@stud.ntnu.no>
2024-04-07 22:47:16 +0200

    Program now reads of zip files in the folder "test_images" and makes 2 different directories, one for txt and one for the images

--- main.py
+++ main.py
@@ -55,21 +55,12 @@ def convert_to_yolo(xml_file, output_folder, index):
             height = (ybr - ytl) / image_height
 
             # Write YOLO-NAS format to text file
-            txt_filename = f"{output_folder}/{index}frame_{frame:06d}.txt"
+            txt_filename = f"{output_folder}/{index}_frame_{frame:06d}.txt"
             with open(txt_filename, 'a') as txt_file:
                 txt_file.write(f"{label} {x_center} {y_center} {width} {height}\n")
 
 
 if __name__ == "__main__":
-
-    zip_files = ["test_images/task_fish_detection-2024_01_28_18_33_21-cvat for video 1.1.zip",
-                 "test_images/task_fish_detection-2024_02_05_18_43_34-cvat for video 1.1.zip",
-                 "test_images/task_fish_detection-2024_02_05_11_51_48-cvat for video 1.1.zip"]
-
-    # zip_file = "test_images/task_fish_detection-2024_01_28_18_33_21-cvat for video 1.1.zip"
-    # zip_file = "test_images/task_fish_detection-2024_02_05_18_43_34-cvat for video 1.1.zip"
-    # zip_file = "test_images/task_fish_detection-2024_02_05_11_51_48-cvat for video 1.1.zip"
-
     output_folder_txt = "yolo_txt"
 
     output_folder_img = "yolo_img"
@@ -80,17 +71,21 @@ if __name__ == "__main__":
     if not os.path.exists(output_folder_img):
         os.makedirs(output_folder_img)
 
-    for i, zip_file in zip_files:
-        extract_folder = "extracted_data" + i
+    files = os.listdir("test_images")
+
+    zip_files = [file for file in files]
+
+    for i, zip_file in enumerate(zip_files):
+        extract_folder = f"extracted_data_{i}"
         if not os.path.exists(extract_folder):
             os.makedirs(extract_folder)
 
-        unzip_files(zip_file, extract_folder)
+        unzip_files(f"test_images/{zip_file}", extract_folder)
         xml_file = f"{extract_folder}/annotations.xml"
         images_folder = f"{extract_folder}/images"
         convert_to_yolo(xml_file, output_folder_txt, i)
         files = os.listdir(images_folder)
 
-        for fileName in enumerate(os.listdir(images_folder)):
+        for fileName in os.listdir(images_folder):
             new_filename = f"{i}_{fileName}"
             shutil.copy2(os.path.join(images_folder, fileName), os.path.join(output_folder_img, new_filename))
